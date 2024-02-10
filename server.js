8e8ee2093cdbf9b316754d48a0b52188045d65d8
Twayfrancis <tawaifrancis@gmail.com>
2024-02-10 23:38:53 +0300

    fix eslint

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const routes = require('./routes');
 // create express app
 const app = express();
 
-
 // set port from env var or default to 5000
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // load routes from routes/index.js
 app.use('/', routes);
 
 // start the server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
