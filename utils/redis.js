72499c9a4d87e9893eb969ed811a4d4742955c82
Twayfrancis <tawaifrancis@gmail.com>
2024-02-10 20:33:34 +0300

    Inside the folder utils, create a file redis.js that contains the class RedisClient.
    RedisClient should have:
    
    the constructor that creates a client to Redis:
    any error of the redis client must be displayed in the console (you should use on(error) of the redis client)
    a function isAlive that returns true when the connection to Redis is a success otherwise, false
    an asynchronous function get that takes a string key as argument and returns the Redis value stored for this key
    an asynchronous function set that takes a string key, a value and a duration in second as arguments to store it in Redis (with an expiration set by the duration argument)
    an asynchronous function del that takes a string key as argument and remove the value in Redis for this key
    After the class definition, create and export an instance of RedisClient called redisClient.

---
+++ utils/redis.js
@@ -0,0 +1,66 @@
+// import the Redis library
+const { resolve } = require('mongodb/lib/core/topologies/read_preference');
+const redis = require('redis');
+
+class RedisClient {
+    constructor() {
+        // create a Redis client
+        this.client = redis.createClient();
+
+        // error handling
+        this.client.on('error', (error) => console.error(`Redis client error: ${error}`));
+
+        // successful conn
+        this.client.on('connect', () => console.log('Redis client connected'));
+
+        // reconn
+        this.client.on('reconnecting', () => console.log('Redis client reconnecting'));
+    }
+    // check if the client is conn and ready
+    isAlive() {
+        return this.client.connected;
+    }
+
+    // get value by key
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    // set value with key and expiration
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (error, reply) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    // delete a key
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (error, reply) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+}
+
+// export an instance of RedisClient
+const redisClient = new RedisClient();
+module.exports = redisClient;
