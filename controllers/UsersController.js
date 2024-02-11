faf25fbc027ba5dea40645eec61db8ef1651fd6f
Twayfrancis <tawaifrancis@gmail.com>
2024-02-11 14:56:11 +0300

    In the file routes/index.js, add a new endpoint:
    POST /users => UsersController.postNew
    Inside controllers, add a file UsersController.js that contains the new endpoint:
    
    POST /users should create a new user in DB:
    
    To create a user, you must specify an email and a password
    If the email is missing, return an error Missing email with a status code 400
    If the password is missing, return an error Missing password with a status code 400
    If the email already exists in DB, return an error Already exist with a status code 400
    The password must be stored after being hashed in SHA1
    The endpoint is returning the new user with only the email and the id (auto generated by MongoDB) with a status code 201
    The new user must be saved in the collection users:
    email: same as the value received
    password: SHA1 value of the value received

---
+++ controllers/UsersController.js
@@ -0,0 +1,49 @@
+const { v4: uuidv4 } = require('uuid');
+const sha1 = require('sha1');
+const DBClient = require('../utils/db');
+
+const { ObjectId } = require('mongodb');
+
+const UsersController = {
+  // POST /users endpoint
+  async postNew(req, res) {
+    const { email, password } = req.body;
+
+    // check if email and pass are provided
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      // check if email exist in DB
+      const existingUser = await DBClient.db.collection('users').findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      // hash password
+      const hashedPassword = sha1(password);
+
+      // create new user obj
+      const newUser = {
+        email,
+        password: hashedPassword,
+        id: uuidv4(), // generate uuid for the user ID
+      };
+
+      // insert new user into db
+      await DBClient.db.collection('users').insertOne(newUser);
+
+      // return new user
+      return res.status(201).json({ id: newUser.id, email: newUser.email });
+    } catch (error) {
+      console.error('Error creating new user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
+
+module.exports = UsersController;
